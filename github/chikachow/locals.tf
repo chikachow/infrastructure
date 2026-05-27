65bf40aa4ff0a64141db6d021862a32de50ed2da
Scott Talbot <s@chikachow.org>
2026-05-27 17:35:30 +1000

    feat(github/chikachow/infrastructure): require pull request approval

--- github/chikachow/locals.tf
+++ github/chikachow/locals.tf
@@ -1,6 +1,8 @@
 locals {
   github_owner = "chikachow"
 
+  atlantis_github_app_slug = "chikachow-atlantis"
+
   terraform_github_app_pem_file_provided = nonsensitive(var.chikachow_terraform_github_app_pem_file != null)
   terraform_github_app_auth_enabled = (
     local.terraform_github_app_pem_file_provided ||
