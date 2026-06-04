e68794412bc142d432440b337ea66723ae488420
Scott Talbot <s@chikachow.org>
2026-06-04 20:44:37 +1000

    refactor(github/chikachow): name integration ids

--- github/chikachow/locals.tf
+++ github/chikachow/locals.tf
@@ -1,6 +1,9 @@
 locals {
   github_owner = "chikachow"
 
+  github_actions_integration_id = 15368
+  atlantis_integration_id       = 3852202
+
   terraform_github_app_pem_file_provided = nonsensitive(var.chikachow_terraform_github_app_pem_file != null)
   terraform_github_app_auth_enabled = (
     local.terraform_github_app_pem_file_provided ||
