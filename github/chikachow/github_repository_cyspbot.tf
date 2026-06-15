2dfd3d8603c7d263302b58cbfa9301a218c3376c
Scott Talbot <s@chikachow.org>
2026-06-15 21:07:04 +1000

    feat(github/chikachow): publish cyspbot repository

--- github/chikachow/github_repository_cyspbot.tf
+++ github/chikachow/github_repository_cyspbot.tf
@@ -2,10 +2,35 @@ module "cyspbot_repository" {
   source = "../../modules/github-repository"
 
   name       = "cyspbot"
-  visibility = "private"
+  visibility = "public"
 
   allow_auto_merge = false
-  allow_forking    = null
+}
+
+module "cyspbot_ruleset_protect_default_branch" {
+  source = "../../modules/github-repository-ruleset-protect-default-branch"
+
+  repository = module.cyspbot_repository.name
+}
+
+module "cyspbot_ruleset_ci" {
+  source = "../../modules/github-repository-ruleset-required-status-checks"
+
+  repository = module.cyspbot_repository.name
+  name       = "ci"
+
+  required_status_checks = [
+    {
+      context        = "ci"
+      integration_id = local.github_actions_integration_id
+    },
+  ]
+}
+
+module "cyspbot_ruleset_require_codeql" {
+  source = "../../modules/github-repository-ruleset-require-codeql"
+
+  repository = module.cyspbot_repository.name
 }
 
 resource "github_repository_environment" "cyspbot_production" {
