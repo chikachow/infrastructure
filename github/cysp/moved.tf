e5516829ab6a67d5442816f7e41c834da1122b39
Scott Talbot <s@chikachow.org>
2026-08-05 08:22:01 +1000

    refactor(github): compose repository actions variables

---
+++ github/cysp/moved.tf
@@ -0,0 +1,49 @@
+moved {
+  from = github_actions_variable.graphql_schema_registry_cyspbot_app_id
+  to   = github_actions_variable.graphql_schema_registry["CYSPBOT_APP_ID"]
+}
+
+moved {
+  from = github_actions_variable.graphql_schema_registry_neon_database_name
+  to   = github_actions_variable.graphql_schema_registry["NEON_DATABASE_NAME"]
+}
+
+moved {
+  from = github_actions_variable.graphql_schema_registry_neon_host
+  to   = github_actions_variable.graphql_schema_registry["NEON_HOST"]
+}
+
+moved {
+  from = github_actions_variable.graphql_schema_registry_neon_parent_branch
+  to   = github_actions_variable.graphql_schema_registry["NEON_PARENT_BRANCH"]
+}
+
+moved {
+  from = github_actions_variable.graphql_schema_registry_neon_project_id
+  to   = github_actions_variable.graphql_schema_registry["NEON_PROJECT_ID"]
+}
+
+moved {
+  from = github_actions_variable.graphql_schema_registry_neon_role_name
+  to   = github_actions_variable.graphql_schema_registry["NEON_ROLE_NAME"]
+}
+
+moved {
+  from = github_actions_variable.terraform_provider_contentful_contentful_environment_id
+  to   = github_actions_variable.terraform_provider_contentful["CONTENTFUL_ENVIRONMENT_ID"]
+}
+
+moved {
+  from = github_actions_variable.terraform_provider_contentful_contentful_organization_id
+  to   = github_actions_variable.terraform_provider_contentful["CONTENTFUL_ORGANIZATION_ID"]
+}
+
+moved {
+  from = github_actions_variable.terraform_provider_contentful_contentful_space_id
+  to   = github_actions_variable.terraform_provider_contentful["CONTENTFUL_SPACE_ID"]
+}
+
+moved {
+  from = github_actions_variable.terraform_provider_contentful_cyspbot_app_id
+  to   = github_actions_variable.terraform_provider_contentful["CYSPBOT_APP_ID"]
+}
