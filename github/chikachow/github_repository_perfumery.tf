9eb0e940f33143fbf7dc75fc3f3d589b19e5b2ec
Scott Talbot <s@chikachow.org>
2026-09-17 10:02:32 +1000

    feat(github): provision private Perfumery repository

---
+++ github/chikachow/github_repository_perfumery.tf
@@ -0,0 +1,10 @@
+module "perfumery_repository" {
+  source = "../../modules/github-repository"
+
+  name        = "perfumery"
+  description = "Perfumery application"
+  visibility  = "private"
+
+  allow_auto_merge = false
+  default_branch   = null
+}
